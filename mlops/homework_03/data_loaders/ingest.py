3f16627a38bb040c7d6272b7187d53a8d5d17970
Aleksei_Terentev2 <aleksei_terentev2@epam.com>
2024-11-26 18:31:57 +0100

    added ingest and transform blocks

---
+++ mlops/homework_03/data_loaders/ingest.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data(*args, **kwargs):
+    
+    
+    df = pd.read_parquet('mlops/data/yellow_tripdata_2023-03.parquet')
+
+    return df
+    
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
