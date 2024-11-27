63ed447d2be2e03fb67973e22144128ef4a147f2
Aleksei Terentev <44712948+teralex79@users.noreply.github.com>
2024-11-27 01:46:45 +0000

    some update

--- mlops/homework_03/data_exporters/register.py
+++ mlops/homework_03/data_exporters/register.py
@@ -3,7 +3,7 @@ import mlflow
 
 EXPERIMENT_NAME = "nyc-yellow-taxi-lin-reg-exp"
 
-mlflow.set_tracking_uri('http://localhost:5001')
+mlflow.set_tracking_uri('http://127.0.0.1:5000/')
 mlflow.set_experiment(EXPERIMENT_NAME)
 
 if 'data_exporter' not in globals():
